9ccc4a7fd42623b34e73dfe91491e563f00d02a6
ahnafi <devsulthon@gmail.com>
2024-06-07 18:14:36 +0700

    add: monitor pages and fix homepage

--- readme.md
+++ readme.md
@@ -3,3 +3,7 @@ link referensi
 [dribble 1](https://dribbble.com/shots/20037874-Landing-Page-Concept)
 
 color darkmode boostsrap 212529 rgb(33,37,41)
+
+info gempa api
+https://data.bmkg.go.id/gempabumi/
+https://data.bmkg.go.id/DataMKG/TEWS/autogempa.json
--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,13 @@
 const lamp = document.getElementById("lamp");
 const toggle = document.getElementById("toggleLamp");
 
-toggle.addEventListener("change",()=>{
+toggle.addEventListener("change", () => {
+  const on = "src/img/lamp-on.svg";
+  const off = "src/img/lamp-off.svg";
 
-    const on = "src/img/lamp-on.svg";
-    const off = "src/img/lamp-off.svg";
-
-    if(lamp.src.includes(off)){
-        lamp.src = on;
-    }else {
-        lamp.src = off;
-    }
-})
+  if (lamp.src.includes(off)) {
+    lamp.src = on;
+  } else {
+    lamp.src = off;
+  }
+});
